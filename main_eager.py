7508e1d7a8bb154635266251e2ecf83e3e3979a8
Ren Mingrui <972931182@qq.com>
2019-04-05 01:10:42 +0800

    增加注释
    1.增加注释，用vscode的better comment插件查看。有突出效果。

--- main_eager.py
+++ main_eager.py
@@ -4,11 +4,16 @@ import os
 
 tf.enable_eager_execution()
 
+'''
+# * 第一步：找到所有图片的路径
+# ? 使用pathlib模块找相对路径？
+'''
 # pro_path = path()
 # image_path = pro_path/'wiki_crop'/'00'
 # image_root = list(image_path.glob('**/*.*'))
 # image_root = [str(i) for i in image_root]
 
+# ? 使用ok模块找绝对路径
 root = os.getcwd()
 root += '\\wiki_crop\\00'
 img_path = []
@@ -16,6 +21,10 @@ for root, dirs, files in os.walk(root):
     for files_name in files:
         img_path.append(root+'\\'+files_name)
 
+'''
+# * 第二步：加载全部图片为tensor（张量）
+# todo 使用其他加载函数 Image.load()
+'''
 def decode_img(path):
     img_raw = tf.read_file(path)
     img_tensor = tf.io.decode_image(img_raw, channels=3)
@@ -28,24 +37,11 @@ img_tensors = []
 for i in img_path:
     img_tensors.append(decode_img(i))
 
-img_tensors = img_tensors[:100]
-img_dataset = tf.data.Dataset.from_tensor_slices(img_tensors)
 
-# def preprocess_image(image):
-#   image = tf.image.decode_jpeg(image, channels=3)
-#   image = tf.image.resize_images(image, [192, 192])
-#   image /= 255.0  # normalize to [0,1] range
-
-#   return image
-
-# image1 = tf.read_file(img_path[0])
-# def load_and_preprocess_image(path):
-#   image = tf.read_file(path)
-#   return preprocess_image(image)
-
-# path_ds = tf.data.Dataset.from_tensor_slices(img_path)
-# image_dataset = path_ds.map(load_and_preprocess_image, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-
+'''
+# * 附加步骤，看一下图片
+# * 开启show()才能看到
+'''
 import matplotlib.pyplot as plt
 
 plt.figure(figsize=(8,8))
@@ -56,8 +52,12 @@ for n,image in enumerate(img_dataset.take(4)):
   plt.xticks([])
   plt.yticks([])
   plt.xlabel(img_path[n])
- 
+#   plt.show()
 
+
+''' 
+# * 第三步：加载图片对应的年龄（来自于mat文件）
+'''
 import mat
 # 组合img_dataset和mat
 # 1.根据img_path重新排mat中age顺序
@@ -72,10 +72,14 @@ for file_path in img_path:
 # for age in mat.age:
 #     if age.get('filename') == '10110600_1985-09-17_2012.jpg':
 #         print(age['age'])
+
+
+'''
+# * 第四步：数据转为dataset,并取前n张（多了爆内存死机）
+# todo 是否可以不转为dataset？直接训练
+'''
+img_tensors = img_tensors[:100]
+img_dataset = tf.data.Dataset.from_tensor_slices(img_tensors)
 age_dataset = age_dataset[:100]
 age_dataset = tf.data.Dataset.from_tensor_slices(age_dataset)
 image_label_ds = tf.data.Dataset.zip((img_dataset, age_dataset))
-
-
-
-a=0
--- model.py
+++ model.py
@@ -1,11 +1,20 @@
 from main_eager import image_label_ds as ds
 import tensorflow as tf
 
+'''
+  # * 第一步：数据shuffle（打乱顺序）+ repeat + batch
+  # ? repeat和batch的作用？
+'''
 ds = ds.apply(
   tf.data.experimental.shuffle_and_repeat(buffer_size=600))
 ds = ds.batch(32)
 ds = ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
+
+'''
+  # * 第二步：数据[0-1]转换到[-1,1]
+  # ? why？ 为什么要转换？教程说是mobilenet要求的，但是这里vgg19也要？
+'''
 def change_range(image,label):
   return 2*image-1, label
 
@@ -14,6 +23,12 @@ image_batch, label_batch = next(iter(ds))
 # ds = tf.keras.applications.vgg19.preprocess_input(image_batch)
 vgg19 = tf.keras.applications.VGG19(include_top=False, input_shape=(192, 192, 3), weights='imagenet')
 
+
+'''
+  # * 第三步：建立sequential序列模型
+  # ? 这里的输出层是Dense?
+  # ? loss和metrics的选择
+'''
 # feature_shape = vgg19(image_batch)
 model = tf.keras.Sequential([
     vgg19,
@@ -24,5 +39,12 @@ model.compile(optimizer=tf.train.AdamOptimizer(),
               loss="mean_absolute_error",
               metrics=["mean_absolute_error"])
 model.summary()
+
+
+'''
+  # * 第四步：开始训练
+  # todo 保存模型，修改steps_per_epoch
+  # todo 增加可视化
+'''
 model.fit(ds, epochs=3, steps_per_epoch=3)
 a=0
