099494d7e4a8f54aff3704f207b69b6ce9ee00a3
Ren Mingrui <972931182@qq.com>
2019-04-23 19:22:27 +0800

    1.服务器上发现必须把model和predict放在一起，否则会出错，错误原因大致应该是多线程造成的问题，具体原因不详
    这是最终稿了，剩下的就是优化工作了
    
    todo：
    ①确认是不是TensorFlow的resize把图片弄出问题了
    ②补充readme

--- jiladahe1997s_website_ml/jiladahe1997s_website_ml/views.py
+++ jiladahe1997s_website_ml/jiladahe1997s_website_ml/views.py
@@ -4,13 +4,10 @@ import tensorflow as tf
 import os
 
 
-model = tf.keras.models.load_model(os.path.abspath('./my_model.h5'))
-model.compile(optimizer=tf.train.AdamOptimizer(), 
-            loss="mean_absolute_error",
-            metrics=["mean_absolute_error"])
+
 @csrf_exempt
 def age_predict(request):
-  # todo 走一遍机器学习
+# todo 走一遍机器学习
   import tensorflow as tf
   import os
   import numpy as np
@@ -19,6 +16,13 @@ def age_predict(request):
   print(os.path.abspath('../my_model.h5'))
   # * 模拟原生的二进制数据
   # img_raw = open(os.path.abspath('./QQ截图20190420203720.png'), 'rb').read()
+  global model
+  model = tf.keras.models.load_model(os.path.abspath('./my_model.h5'))
+  model.compile(optimizer=tf.train.AdamOptimizer(), 
+              loss="mean_absolute_error",
+              metrics=["mean_absolute_error"])
+  global graph
+  graph = tf.get_default_graph()
   img_base64_header, img_base64 = json.loads(request.body)['base64_img'].split(",",1)
   raw = base64.decodestring(img_base64.encode('utf-8'))
   img_base64_websafe = base64.urlsafe_b64encode(raw)
@@ -26,5 +30,6 @@ def age_predict(request):
   # * 模拟结束
   image_tensor = tf.cast(tf.image.resize_images(tf.io.decode_jpeg(img_raw, channels=3), [192,192]),tf.float32)
   image = tf.expand_dims(image_tensor, axis=0)
-  age = model.predict(image, steps=1) 
+  age = model.predict(image, steps=1)
+  model._make_predict_function()
   return HttpResponse(age) 
